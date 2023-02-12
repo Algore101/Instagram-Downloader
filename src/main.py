90a5cf1c9de6e425fafdcb7aa22fc38fc87e740c
Jaydon Walters <jaydonwalters21@gmail.com>
2023-02-12 19:11:58 +0200

    Restructure GUI

--- src/main.py
+++ src/main.py
@@ -31,106 +31,93 @@ class DesktopApp(tk.Tk):
         super().__init__()
         # Set properties
         self.resizable(False, False)
-        self.title("Instagram Post Downloader")
+        self.title("InstaSaver: Instagram Post Downloader")
 
-        # Main frame to hold other widgets
-        self.main_frame = None
+        # URL input widgets
+        self.url_frame = None               # Frame to group widgets together
+        self.url_input = None               # Entry field to type the URL
+        self.url_sv = None                  # StringVar to hold contents of URL entry field
+        self.clear_url_button = None        # Button to clear URL entry field
+        self.get_post_button = None         # Button to fetch post at URL
 
-        # Input fields
-        self.url_input = None
-        self.url = None
-        self.dir_input = None
-        self.directory = None
+        # Preview post widgets
+        self.preview_frame = None           # Frame to group widgets together
 
-        # Audio only option for videos
-        self.audio_only = None
-        self.audio_only_checkbox = None
+        # Download options widgets
+        self.options_frame = None           # Frame to group widgets together
+        self.dir_input = None               # Entry field to type the directory
+        self.directory_sv = None            # StringVar to hold contents of directory entry field
+        self.browse_button = None           # Button to browse directory
+        self.audio_only_checkbox = None     # Checkbox to download audio only from video posts
+        self.audio_only_iv = None           # IntVar to hold contents of "audio only" checkbox
+        self.audio_types = ["wav", "mp3"]   # List of supported audio formats
+        self.audio_type_selection = None    # Dropdown list containing the supported audio formats
+        self.selected_audio_type_sv = None  # StringVar to hold the currently selected audio format
 
-        # Audio type
-        self.audio_types = ["wav", "mp3"]
-        self.selected_audio_type = None
-        self.audio_type_selection = None
-
-        # Buttons
-        self.browse_button = None
-        self.download_button = None
-        self.clear_url_button = None
-        self.get_post_button = None
-
-        # Toggleable labels
-        self.ready_label = None
+        # Download post widgets
+        self.download_frame = None          # Frame to group widgets together
+        self.ready_label = None             # Label to notify when post is ready to be downloaded
+        self.download_button = None         # Button to download post
 
         # Create GUI
         self.create_gui()
 
     def create_gui(self) -> None:
         """
-        Creates GUI elements and packs them on-screen.
+        Creates GUI elements and lays them out on-screen.
         """
-        # Create main frame for holding all widgets
-        self.main_frame = tk.Frame(self, width=800, height=600, padx=20, pady=20)
-        self.main_frame.pack()
+        # Title label
+        ttk.Label(self, text="InstaSaver").grid(column=0, row=0)
 
-        # Create widgets to do with the URL
-        self.url = tk.StringVar()  # Stores the text of the URL input field
-        self.url.trace_add("write", self.make_get_button_available)
-        self.url_input = ttk.Entry(self.main_frame, width=50, textvariable=self.url)  # URL input
-        self.clear_url_button = ttk.Button(self.main_frame, text="Clear",
-                                           command=lambda: self.clear_input_contents(self.url))  # Clear URL button
+        # URL input widgets - Creation
+        self.url_frame = tk.Frame(self, padx=20, pady=5)
+        self.url_sv = tk.StringVar()
+        self.url_sv.trace_add("write", self.make_get_button_available)
+        self.url_input = ttk.Entry(self.url_frame, width=50, textvariable=self.url_sv)
+        self.clear_url_button = ttk.Button(self.url_frame, text="Clear",
+                                           command=lambda: self.clear_input_contents(self.url_sv))
+        self.get_post_button = ttk.Button(self.url_frame, text="Get post", command=self.get_post, state="disabled")
 
-        # Create widgets to do with the directory
-        self.directory = tk.StringVar()  # Stores the text of the directory input field
-        self.directory.trace_add("write", self.make_get_button_available)
-        self.dir_input = ttk.Entry(self.main_frame, width=50, textvariable=self.directory)  # Directory input
-        self.browse_button = ttk.Button(self.main_frame, text="Browse",
-                                        command=self.browse_directory)  # Browse directory button
+        # URL input widgets - Layout
+        self.url_frame.grid(row=1, column=0)
+        ttk.Label(self.url_frame, text="URL:").grid(row=0, column=0)
+        self.url_input.grid(row=0, column=1, padx=5)
+        self.clear_url_button.grid(row=0, column=2)
+        self.get_post_button.grid(row=1, column=0, columnspan=3)
 
-        # Create get post button
-        self.get_post_button = ttk.Button(self.main_frame, text="Get post", command=self.get_post, state="disabled")
-
-        # Create "audio only" checkbox
-        self.audio_only = tk.IntVar()  # Stores the value of the checkbox
-        self.audio_only_checkbox = ttk.Checkbutton(self.main_frame, text="Download audio only",
-                                                   variable=self.audio_only, command=self.toggle_audio_type_selector)
-
-        # Create audio type selector
-        self.selected_audio_type = tk.StringVar()
-        self.audio_type_selection = ttk.OptionMenu(self.main_frame, self.selected_audio_type, self.audio_types[0],
+        # Download options widgets - Creation
+        self.options_frame = tk.Frame(self, padx=20, pady=5)
+        self.directory_sv = tk.StringVar()
+        self.directory_sv.trace_add("write", self.make_get_button_available)
+        self.dir_input = ttk.Entry(self.options_frame, width=50, textvariable=self.directory_sv)
+        self.browse_button = ttk.Button(self.options_frame, text="Browse", command=self.browse_directory)
+        self.audio_only_iv = tk.IntVar()
+        self.audio_only_checkbox = ttk.Checkbutton(self.options_frame, text="Download audio only",
+                                                   variable=self.audio_only_iv, command=self.toggle_audio_type_selector)
+        self.selected_audio_type_sv = tk.StringVar()
+        self.audio_type_selection = ttk.OptionMenu(self.options_frame, self.selected_audio_type_sv, self.audio_types[0],
                                                    *self.audio_types)
 
-        # Create "ready to download" label
-        self.ready_label = tk.Label(self.main_frame, text="Post is ready to be downloaded!")
+        # Download options widgets - Layout
+        self.options_frame.grid(row=2, column=0)
+        ttk.Label(self.options_frame, text="Download options:").grid(row=0, column=0, columnspan=3)
+        ttk.Label(self.options_frame, text="Directory:").grid(column=0, row=1)
+        self.dir_input.grid(column=1, row=1)
+        self.browse_button.grid(column=2, row=1)
+        self.audio_only_checkbox.grid(column=0, row=2, columnspan=2)
+        self.audio_type_selection.grid(column=2, row=2)
 
-        # Create download button
-        self.download_button = ttk.Button(self.main_frame, text="DOWNLOAD", command=self.download, state="disabled")
+        # Download post widgets - Creation
+        self.download_frame = tk.Frame(self, padx=20, pady=5)
+        self.download_button = ttk.Button(self.download_frame, text="DOWNLOAD", command=self.download, state="disabled")
 
-        # Layout widgets
-        # Pack title label
-        ttk.Label(self.main_frame, text="Post Downloader").grid(column=0, row=0, columnspan=3)  # Title label
-
-        # Pack URL widgets
-        ttk.Label(self.main_frame, text="URL:").grid(column=0, row=1)  # URL label
-        self.url_input.grid(column=1, row=1, padx=5)  # URL input
-        self.clear_url_button.grid(column=2, row=1)  # Clear URL button
-
-        # Pack directory widgets
-        ttk.Label(self.main_frame, text="Directory:").grid(column=0, row=2)  # Directory label
-        self.dir_input.grid(column=1, row=2)  # Directory input
-        self.browse_button.grid(column=2, row=2)  # Pick directory button
-
-        # Pack widgets related to downloading
-        self.get_post_button.grid(column=0, row=3, columnspan=3)  # Get post button
-        self.audio_only_checkbox.grid(column=0, row=4, columnspan=2)  # Audio only checkbox
-        self.audio_type_selection.grid(column=2, row=4)  # Audio type dropdown menu
-        self.ready_label.grid(column=0, row=5, columnspan=3)    # "Ready to download" message
-        self.download_button.grid(column=0, row=6, columnspan=3)  # Download button
+        # Download post widgets - Layout
+        self.download_frame.grid(row=3, column=0)
+        self.download_button.grid(column=0, row=6, columnspan=3)
 
         # Hide widgets
-        self.audio_only_checkbox.grid_remove()
-        self.audio_only_checkbox.config(state="disabled")
-        self.audio_type_selection.grid_remove()
-        self.audio_type_selection.config(state="disabled")
-        self.ready_label.grid_remove()
+        self.options_frame.grid_remove()
+        self.download_frame.grid_remove()
 
     def browse_directory(self) -> str:
         """
@@ -148,7 +135,7 @@ class DesktopApp(tk.Tk):
         TODO: Add low-res image preview
         """
         # Display download options based on the file type
-        if downloader.get_media_type(self.url.get()) == "mp4":
+        if downloader.get_media_type(self.url_sv.get()) == "mp4":
             # Show "audio only" checkbox
             self.audio_only_checkbox.config(state="normal")
             self.audio_only_checkbox.grid()
@@ -170,7 +157,7 @@ class DesktopApp(tk.Tk):
         """
         Toggle the dropdown list for the audio type
         """
-        if self.audio_only.get() == 1:
+        if self.audio_only_iv.get() == 1:
             # Enable type selector
             self.audio_type_selection.config(state="normal")
         else:
@@ -189,11 +176,11 @@ class DesktopApp(tk.Tk):
         try:
             # Check for audio only
             logging.debug("Check for audio only...\n\t%s\n\t%s", self.audio_only_checkbox["state"],
-                          self.audio_only.get())
-            if self.audio_only_checkbox["state"] == "normal" and self.audio_only.get() == 1:
-                logging.debug("Downloading audio (%s) only...", self.selected_audio_type.get())
+                          self.audio_only_iv.get())
+            if self.audio_only_checkbox["state"] == "normal" and self.audio_only_iv.get() == 1:
+                logging.debug("Downloading audio (%s) only...", self.selected_audio_type_sv.get())
                 # Download audio only
-                downloader.download_instagram_post(url, filepath, self.selected_audio_type.get())
+                downloader.download_instagram_post(url, filepath, self.selected_audio_type_sv.get())
             else:
                 logging.debug("Downloading video/audio...")
                 downloader.download_instagram_post(url, filepath)
@@ -210,8 +197,8 @@ class DesktopApp(tk.Tk):
         :return: None
         """
         if widget is None:
-            self.url.set("")
-            self.directory.set("")
+            self.url_sv.set("")
+            self.directory_sv.set("")
             # Remove ready message
             self.ready_label.grid_remove()
             # Focus on URL entry
@@ -220,7 +207,7 @@ class DesktopApp(tk.Tk):
             widget.set("")
 
     def make_get_button_available(self, *args) -> None:
-        if self.url.get() != "" and self.directory.get() != "":
+        if self.url_sv.get() != "" and self.directory_sv.get() != "":
             # Enable get post button
             self.get_post_button.config(state="normal")
         else:
